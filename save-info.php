c8b919d0167280257fcc950840b71a1433b90b35
Josh Bruce <92905423+JoshCBruce@users.noreply.github.com>
2023-03-29 22:23:06 +0100

    Delete save-info.php

--- save-info.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (isset($_POST['data'])) {
-	$data = $_POST['data'];
-	$filename = date('Y-m-d_H-i-s') . '.txt';
-	$file = fopen('logs/' . $filename, 'w');
-	fwrite($file, $data);
-	fclose($file);
-}
-?>
